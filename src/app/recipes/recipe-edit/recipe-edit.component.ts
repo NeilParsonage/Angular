d9d36f2513cef5dfda2154744de954dc51b6229f
Neil Parsonage <neil.parsonage.extern@sonepar.de>
2020-06-11 14:53:38 +0200

    Reactive Recipe form with Ingredients working

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
+import { RecipeService } from './../recipe.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -10,16 +12,63 @@ export class RecipeEditComponent implements OnInit {
 
   id: number;
   editMode: boolean = false;
-  constructor(private route: ActivatedRoute) { }
+  recipeForm: FormGroup;
+
+
+  constructor(private route: ActivatedRoute,
+    private recipeService: RecipeService) { 
+
+    }
 
   ngOnInit(): void {
     this.route.params.subscribe(
       (params: Params) => {
         this.id = +params['id'];
         this.editMode = params['id'] != null;
-        console.log('edit mode '+ this.editMode );
+        this.initForm();
       }
     )
   }
+
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
+
+private initForm() {
+
+  let recipeName = '';
+  let recipeImagePath = '';
+  let recipeDescription = '';
+  let recipeIngredients = new FormArray([]);
+
+  if (this.editMode) {
+    const recipe = this.recipeService.getRecipe(this.id);
+    recipeName = recipe.name;
+    recipeImagePath = recipe.imagePath;
+    recipeDescription = recipe.description;
+    if (recipe['ingredients']) {
+      for ( let ingredient of recipe.ingredients) {
+        recipeIngredients.push(
+          new FormGroup({
+            'name': new FormControl(ingredient.name),
+            'amount': new FormControl(ingredient.amount)
+          })
+        );
+      }
+    }
+
+  }
+  this.recipeForm = new FormGroup({
+    'name': new FormControl(recipeName),
+    'imagePath': new FormControl(recipeImagePath),
+    'description': new FormControl(recipeDescription),
+    'ingredients': recipeIngredients
+
+  });
+}
+
+getControls() {
+  return(<FormArray>this.recipeForm.get('ingredients')).controls;
+}
 
 }
